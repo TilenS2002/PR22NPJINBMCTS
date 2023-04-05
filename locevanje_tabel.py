46236bb9cd4bacf115bdb6c47a078b07f7ad8512
niny8888 <niny.plevnik@gmail.com>
2023-04-05 10:44:18 +0200

    kategorizacija tabele
    Ker je tabela velika, smo kategorično ločili tabelo na več manjših tabel, koda urejanja tabela je zapisana v locevanje_tabel in pripravljena za kasnejše popravke v tabelah

---
+++ locevanje_tabel.py
@@ -0,0 +1,50 @@
+import pandas as pd
+
+# Read in the data from the original file
+df = pd.read_csv('baze\pn2022.csv', delimiter=';')
+
+# koordinate tabela
+new_df = df[['ZaporednaStevilkaOsebeVPN', 'GeoKoordinataX', 'GeoKoordinataY']]
+new_df = new_df.drop(new_df[(new_df['GeoKoordinataX'] == 0) & (new_df['GeoKoordinataY'] == 0)].index)
+new_df.to_csv('baze\manjse\Koordinate.csv', index=False)
+
+# čas
+month_dict={'jan':'01', 'feb':'02', 'mar':'03', 'apr':'04', 'maj':'05', 'jun':'06', 'jul':'07', 'avg':'08', 'sep':'09', 'okt':'10', 'nov':'11', 'dec':'12'}
+
+for key in month_dict.keys():
+    df['UraPN'] = df['UraPN'].str.replace(f'{key}.', f'{month_dict[key]}.')
+    df['UraPN'] = df['UraPN'].str.replace(f'.{key}', f'.{month_dict[key]}')
+
+time_df=df[['ZaporednaStevilkaOsebeVPN','DatumPN','UraPN']]
+time_df.to_csv('baze\manjse\Cas.csv', index=False)
+
+# kraj
+kraj_df=df[['ZaporednaStevilkaOsebeVPN','UpravnaEnotaStoritve','VNaselju','Lokacija','OpisKraja']]
+kraj_df.to_csv('baze\manjse\Kraj.csv', index=False)
+
+# nesreča
+nesreca_df= df[['ZaporednaStevilkaOsebeVPN', 'VzrokNesrece', 'TipNesrece']]
+nesreca_df.to_csv('baze\manjse\Nesreca.csv', index=False)
+
+# cesta
+cesta_df= df[['ZaporednaStevilkaOsebeVPN', 'VrstaCesteNaselja']]
+cesta_df.to_csv('baze\manjse\Cesta.csv', index=False)
+
+# faktorji cestisca
+faktorji_df= df[['ZaporednaStevilkaOsebeVPN', 'VremenskeOkoliscine', 'StanjePrometa', 'StanjeVozisca', 'VrstaVozisca']]
+faktorji_df.to_csv('baze\manjse\FaktorCest.csv', index=False)
+
+# faktorji osebe
+osebniF_df= df[['ZaporednaStevilkaOsebeVPN', 'UporabaVarnostnegaPasu', 'VrednostAlkotesta', 'VrednostStrokovnegaPregleda']]
+osebniF_df.to_csv('baze\manjse\FaktorOseb.csv', index=False)
+
+# o osebi
+df['MeseciIzpita'] = (df['VozniskiStazVLetih'] * 12) + df['VozniskiStazVMesecih']
+osebe_df= df[['ZaporednaStevilkaOsebeVPN', 'Povzrocitelj', 'Starost', 'Spol', 'Drzavljanstvo', 'VrstaUdelezenca', 'PoskodbaUdelezenca', 'MeseciIzpita']]
+osebe_df.to_csv('baze\manjse\Osebe.csv', index=False)
+
+#na splošno
+splosno_df=df[[]]
+splosno_df.to_csv('baze\manjse\Splosno.csv', index=False)
+
+
