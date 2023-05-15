758c135a20da8e2014124384d272fb884677a204
pradles <jernej.ivancic2002@gmail.com>
2023-05-15 17:39:27 +0200

    dodan RFE in popravlen rndForest

--- Analiza.py
+++ Analiza.py
@@ -1,3 +1,9 @@
+#########################################################
+#####                                               #####
+#####         RandomForest Prediction Model         #####
+#####                                               #####
+#########################################################
+
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
@@ -5,23 +11,33 @@ from sklearn.metrics import accuracy_score, classification_report
 
 # Load and merge the datasets
 cas_df = pd.read_csv('baze/manjse/Cas.csv')
-faktoroseb_df = pd.read_csv('baze/manjse/Osebe.csv')
+osebe_df = pd.read_csv('baze/manjse/Osebe.csv')
 nesreca_df = pd.read_csv('baze/manjse/Nesreca.csv')
 splosno_df = pd.read_csv('baze/manjse/Splosno.csv')
+cesta_df = pd.read_csv('baze/manjse/Cesta.csv')
+faktor_cest_df = pd.read_csv('baze/manjse/FaktorCest.csv')
+faktor_oseb_df = pd.read_csv('baze/manjse/FaktorOseb.csv')
 
-merged_df = cas_df.merge(faktoroseb_df, on='ZaporednaStevilkaOsebeVPN')
+merged_df = cas_df.merge(osebe_df, on='ZaporednaStevilkaOsebeVPN')
 merged_df = merged_df.merge(nesreca_df, on='ZaporednaStevilkaOsebeVPN')
 merged_df = merged_df.merge(splosno_df, on='ZaporednaStevilkaOsebeVPN')
+merged_df = merged_df.merge(cesta_df, on='ZaporednaStevilkaOsebeVPN')
+merged_df = merged_df.merge(faktor_cest_df, on='ZaporednaStevilkaOsebeVPN')
+merged_df = merged_df.merge(faktor_oseb_df, on='ZaporednaStevilkaOsebeVPN')
 
 print(merged_df.columns)
-selected_features = ['VzrokNesrece', 'TipNesrece', 'Starost', 'Spol', 'Drzavljanstvo', 'PoskodbaUdelezenca']
+selected_features = ['UraPN', 'Starost', 'VrstaUdelezenca',
+       'MeseciIzpita', 'VzrokNesrece', 'TipNesrece', 'VrstaCesteNaselja',
+       'VremenskeOkoliscine', 'StanjePrometa']
 target_variable = 'KlasifikacijaNesrece'
 
 X = merged_df[selected_features]
 y = merged_df[target_variable]
 
 #Perform  encoding on categorical variables
-X_encoded = pd.get_dummies(X, columns=['VzrokNesrece', 'TipNesrece', 'Spol', 'Drzavljanstvo', 'PoskodbaUdelezenca'])
+X_encoded = pd.get_dummies(X, columns=['UraPN', 'Starost', 'VrstaUdelezenca',
+       'MeseciIzpita', 'VzrokNesrece', 'TipNesrece', 'VrstaCesteNaselja',
+       'VremenskeOkoliscine', 'StanjePrometa'])
 
 #Razdelitev testne in učne
 X_train, X_test, y_train, y_test = train_test_split(X_encoded, y, test_size=0.2, random_state=42)
@@ -39,6 +55,6 @@ y_pred = clf.predict(X_test)
 accuracy = accuracy_score(y_test, y_pred)
 classification_rep = classification_report(y_test, y_pred)
 
-print("Accuracy:", accuracy)
+print("Accuracy:", accuracy) #current Accuracy: 0.790845710821461  15.5. 17:40
 print("Classification Report:")
 print(classification_rep)
