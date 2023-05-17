053d217679a533c75f1f47d8e9d3a85c638fd85b
niny8888 <96949730+niny8888@users.noreply.github.com>
2023-05-17 21:38:34 +0200

    popravki, normalizacija

--- osebe.py
+++ osebe.py
@@ -53,16 +53,16 @@ print(f"Correlation coefficient: {corr_coef_h:.2f}")
 print('P-value:',p_value_h)
 
 #graf dobljenih rezultatov razmera hitrost in leto izpita
-sns.set_style('whitegrid')
-sns.regplot(x=meseci_izpita_counts_hitrost.index, y=meseci_izpita_counts_hitrost,scatter_kws={'color': 'grey'}, line_kws={'color': 'orange'})
-plt.title('Razmerje med meseci izpita povzročitelja in številom nesreč povzročenih zaradi prekomerne hitrosti')
-plt.xlabel('Meseci izpita')
-plt.ylabel('Stevilo nesreč')
+# sns.set_style('whitegrid')
+# sns.regplot(x=meseci_izpita_counts_hitrost.index, y=meseci_izpita_counts_hitrost,scatter_kws={'color': 'grey'}, line_kws={'color': 'orange'})
+# plt.title('Razmerje med meseci izpita povzročitelja in številom nesreč povzročenih zaradi prekomerne hitrosti')
+# plt.xlabel('Meseci izpita')
+# plt.ylabel('Stevilo nesreč')
 
-plt.annotate('r = {:.2f}'.format(corr_coef_h), xy=(0.65, 0.94) , xycoords='axes fraction', fontsize=12)
-plt.annotate('p = {:.2e}'.format(p_value_h), xy=(0.65, 0.88) , xycoords='axes fraction', fontsize=12)
+# plt.annotate('r = {:.2f}'.format(corr_coef_h), xy=(0.65, 0.94) , xycoords='axes fraction', fontsize=12)
+# plt.annotate('p = {:.2e}'.format(p_value_h), xy=(0.65, 0.88) , xycoords='axes fraction', fontsize=12)
 
-plt.show()
+# plt.show()
 
 #############################################################################################################
 #                                                                                                           #
@@ -90,11 +90,35 @@ vec_kot_20 = sitnFolk[mask_vec_kot_20]
 layout = [leto_izpita['MeseciIzpita'].count(), leti2_voznik['MeseciIzpita'].count(), med_2_in_5['MeseciIzpita'].count(), med_5_in_10['MeseciIzpita'].count(), med_10_in_20['MeseciIzpita'].count(), vec_kot_20['MeseciIzpita'].count()]
 grupe = ["1 leto", "2 leti", "2 - 5 let", "5 - 10 let", "10 - 20 let", "vec kot 20 let"]
 
-plt.bar(grupe, layout, color='grey')
-plt.title('Količina PN zaradi neupoštevanja pravil o prednosti, po starostnih skupinah')
-plt.xlabel('Starostne skupine')
-plt.ylabel('količina')
-plt.show()
+total_count= len(sitnFolk)
+
+leto_izpita_percentage = len(leto_izpita) / total_count * 100
+leti2_voznik_percentage = len(leti2_voznik) / total_count * 100
+med_2_in_5_percentage = len(med_2_in_5) / total_count * 100
+med_5_in_10_percentage = len(med_5_in_10) / total_count * 100
+med_10_in_20_percentage = len(med_10_in_20) / total_count * 100
+vec_kot_20_percentage = len(vec_kot_20) / total_count * 100
+
+# Normalizing the data
+normalized_leto_izpita_percentage = leto_izpita_percentage / (leto_izpita_percentage + leti2_voznik_percentage + med_2_in_5_percentage + med_5_in_10_percentage + med_10_in_20_percentage + vec_kot_20_percentage)
+normalized_leti2_voznik_percentage = leti2_voznik_percentage / (leto_izpita_percentage + leti2_voznik_percentage + med_2_in_5_percentage + med_5_in_10_percentage + med_10_in_20_percentage + vec_kot_20_percentage)
+normalized_med_2_in_5_percentage = med_2_in_5_percentage / (leto_izpita_percentage + leti2_voznik_percentage + med_2_in_5_percentage + med_5_in_10_percentage + med_10_in_20_percentage + vec_kot_20_percentage)
+normalized_med_5_in_10_percentage = med_5_in_10_percentage / (leto_izpita_percentage + leti2_voznik_percentage + med_2_in_5_percentage + med_5_in_10_percentage + med_10_in_20_percentage + vec_kot_20_percentage)
+normalized_med_10_in_20_percentage = med_10_in_20_percentage / (leto_izpita_percentage + leti2_voznik_percentage + med_2_in_5_percentage + med_5_in_10_percentage + med_10_in_20_percentage + vec_kot_20_percentage)
+normalized_vec_kot_20_percentage = vec_kot_20_percentage / (leto_izpita_percentage + leti2_voznik_percentage + med_2_in_5_percentage + med_5_in_10_percentage + med_10_in_20_percentage + vec_kot_20_percentage)
+
+print("Percentage of leti_izpita:", normalized_leto_izpita_percentage)
+print("Percentage of leti2_voznik:", normalized_leti2_voznik_percentage)
+print("Percentage of med_2_in_5:", normalized_med_2_in_5_percentage)
+print("Percentage of med_5_in_10:", normalized_med_5_in_10_percentage)
+print("Percentage of med_10_in_20:", normalized_med_10_in_20_percentage)
+print("Percentage of vec_kot_20:", normalized_vec_kot_20_percentage)
+
+# plt.bar(grupe, layout, color='grey')
+# plt.title('Količina PN zaradi neupoštevanja pravil o prednosti, po starostnih skupinah')
+# plt.xlabel('Starostne skupine')
+# plt.ylabel('količina')
+# plt.show()
 
 # print("Length of leto_izpita:", len(leto_izpita))
 # print("Length of leti2_voznik:", len(leti2_voznik))
@@ -111,16 +135,16 @@ print(f"Correlation coefficient: {corr_coef:.2f}")
 print('P-value:',p_value)
 
 #graf dobljenih rezultatov
-sns.set_style('whitegrid')
-sns.regplot(x=meseci_izpita_counts.index, y=meseci_izpita_counts,scatter_kws={'color': 'grey'}, line_kws={'color': 'orange'})
-plt.title('Razmerje med meseci izpita povzročitelja in številom nesreč')
-plt.xlabel('Meseci izpita')
-plt.ylabel('Stevilo nesrec')
+# sns.set_style('whitegrid')
+# sns.regplot(x=meseci_izpita_counts.index, y=meseci_izpita_counts,scatter_kws={'color': 'grey'}, line_kws={'color': 'orange'})
+# plt.title('Razmerje med meseci izpita povzročitelja in številom nesreč')
+# plt.xlabel('Meseci izpita')
+# plt.ylabel('Stevilo nesrec')
 
-plt.annotate('r = {:.2f}'.format(corr_coef), xy=(0.65, 0.94) , xycoords='axes fraction', fontsize=12)
-plt.annotate('p = {:.2e}'.format(p_value), xy=(0.65, 0.88) , xycoords='axes fraction', fontsize=12)
+# plt.annotate('r = {:.2f}'.format(corr_coef), xy=(0.65, 0.94) , xycoords='axes fraction', fontsize=12)
+# plt.annotate('p = {:.2e}'.format(p_value), xy=(0.65, 0.88) , xycoords='axes fraction', fontsize=12)
 
-plt.show()
+# plt.show()
 
 
 
@@ -178,17 +202,17 @@ sizes = percentages.values
 def my_autopct(pct):
     return '{:.1f}%'.format(pct) if pct >= 2 else ''
 
-colors = ['#FF8533','grey', '#FF983E', '#FFAA48', '#FFB34D',  '#FFBC52','#FFCE5C' ]
-explode = [0.1 if size > sum(sizes)*0.03 else 0 for size in sizes] # explode only if size is greater than 3%
+# colors = ['#FF8533','grey', '#FF983E', '#FFAA48', '#FFB34D',  '#FFBC52','#FFCE5C' ]
+# explode = [0.1 if size > sum(sizes)*0.03 else 0 for size in sizes] # explode only if size is greater than 3%
 
-fig, ax = plt.subplots(figsize=(10, 6))
-ax.pie(sizes, labels=labels,autopct=my_autopct, colors=colors, startangle=90, explode=explode, pctdistance=0.8) # type: ignore
-ax.axis('equal') # equal aspect ratio ensures that pie is drawn as a circle
-ax.set_title('Udeleženci v prometni nesreči',y=1.08)
+# fig, ax = plt.subplots(figsize=(10, 6))
+# ax.pie(sizes, labels=labels,autopct=my_autopct, colors=colors, startangle=90, explode=explode, pctdistance=0.8) # type: ignore
+# ax.axis('equal') # equal aspect ratio ensures that pie is drawn as a circle
+# ax.set_title('Udeleženci v prometni nesreči',y=1.08)
 
 # adjust the spacing between subplots to move the pie chart to the left
-plt.subplots_adjust(top=0.85)
-plt.show()
+# plt.subplots_adjust(top=0.85)
+# plt.show()
 
 
 
@@ -225,10 +249,13 @@ starSkupine = [
     e_50_59['Starost'].count() / len(skupi),
     e_60_69['Starost'].count() / len(skupi),
     vec_ku_70['Starost'].count() / len(skupi)
-]# print(starSkupine)
+] 
+normalized_starSkupine = [value / sum(starSkupine) for value in starSkupine]
+
+print("Normalized starSkupine:", normalized_starSkupine)
 StarLabli = ["18 - 25 let", "25 - 30 let", "30 - 39 let", "40 - 49 let", "50 - 59 let", "60 - 69 let", "vec kot 70 let"]
 
-plt.bar(StarLabli, starSkupine, color='grey')
+plt.bar(StarLabli, normalized_starSkupine, color='grey')
 plt.title('Količina PN po starostnih skupinah')
 plt.xlabel('Starostne skupine')
 plt.ylabel('količina')
@@ -240,14 +267,33 @@ Z18_25 = e_18_25[e_18_25['Spol'].str.contains('ŽENSKI')].count()
 mzPN = [M18_25['Spol'] / len(e_18_25), Z18_25['Spol'] / len(e_18_25)]
 listic = ["Moški", "Ženske"]
 
-plt.bar(listic, mzPN, color='grey')
-plt.title('Količina PN po spolu v starostni skupini 18 - 25')
-plt.xlabel('Spol')
-plt.ylabel('količina')
-plt.show()
+# plt.bar(listic, mzPN, color='grey')
+# plt.title('Količina PN po spolu v starostni skupini 18 - 25')
+# plt.xlabel('Spol')
+# plt.ylabel('količina')
+# plt.show()
 
 
-# print("Moški vdeleženci v PN: ",M18_25['Spol'])
-# print("Ženske vdeleženke v PN: ",Z18_25['Spol'])
+print("Moški vdeleženci v PN: ",M18_25['Spol'])
+print("Ženske vdeleženke v PN: ",Z18_25['Spol'])
 
 
+
+# | Leta izpita| procent nesreč|
+# |-|-|
+# | <1 | 0.047% |
+# |1-2| 0.022%|
+# | 2-5 | 0.072%|
+# |5-10| 0.109%|
+# |10-20| 0.19%|
+# |20<| 0.556|
+
+# |starostna skupina | procent nesreč|
+# |-|-|
+# | 18-25 | 0.179%|
+# |25-30|0.095%|
+# |30-39| 0.164%|
+# |40-49| 0.179%|
+# |50-59| 0.149%|
+# |60-69| 0.116%|
+# |70<| 0.115%|
